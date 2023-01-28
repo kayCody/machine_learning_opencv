dd558703035376c5a6c54b42f2bff65789008d5f
idevpluxinc <nanaqwame763@gmail.com>
2023-01-28 13:01:31 +0000

    first push on machine language

---
+++ tests/file.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+import sys
+
+image = cv.imread(cv.samples.findFile("/Users/apple/Downloads/IMG_4936.jpg"))
+
+if image is None:
+    sys.exit("could not load image")
+else:
+    cv.imshow("Display window", image)
+    k = cv.waitKey(0)
+
+    if k == ord("s"):
+        cv.imwrite("new_image.jpg", image)
